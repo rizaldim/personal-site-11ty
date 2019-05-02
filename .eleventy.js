253c5138a4cbad26e28d0c8ad8c2c73b8979080c
Rizaldi Martaputra <rizaldi.m@gmail.com>
2019-05-03 00:30:30 +0700

    Use css directory

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
-const CleanCSS = require("clean-css");
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addFilter("cssmin", function(code) {
-    return new CleanCSS({}).minify(code).styles;
-  });
+  eleventyConfig.addPassthroughCopy("css");
+  return {
+    passthroughFileCopy: true
+  };
 };
