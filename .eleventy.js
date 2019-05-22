fc25435584945acf733cc92dde741a875635d358
Rizaldi Martaputra <rizaldi.m@gmail.com>
2019-05-22 22:00:10 +0700

    Add filter: readableDate

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,15 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
+
   eleventyConfig.addPassthroughCopy("css");
+
+  eleventyConfig.addFilter("readableDate", (dateObj, fmt) => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat(fmt);
+  });
+
   return {
     passthroughFileCopy: true
   };
+
 };
